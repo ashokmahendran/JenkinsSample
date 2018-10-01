63eb53c9a1732a7ca2145f8664991b7fbd3fa58b
Shankkar Ganesh S <=>
2018-10-01 10:10:56 +0530

    Jenkins Sample Project

---
+++ index.js
@@ -0,0 +1,23 @@
+let express=require('express');
+const app=express();
+var cors = require('cors');
+app.use(cors());
+
+app.get('/login', (req, res) => {
+    console.log('Here');
+    
+    res.send('Hello World!')
+
+})
+app.get('/', (req, res) => {
+    console.log('Here');
+    
+    res.send('Hello World ///////')
+
+})
+app.listen(8081,(err)=>{
+    if(err){
+        console.log('Server Starts failed',err.msg);
+    }
+    console.log('Listening 7070');
+})
